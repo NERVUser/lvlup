0e20aa558f3bb68161cef6a343f55f55d65473aa
Neil S <88115444+jul158@users.noreply.github.com>
2024-11-15 22:46:02 -0500

    Update WorkoutResults.tsx

---
+++ frontend/src/WorkoutResults/WorkoutResults.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '.\WorkoutResults.css';
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
+
+function WorkoutResults() {
+    return(
+    <TableContainer="WorkoutResults">
+        <TableHead="WorkoutHeader">
+            <TableRow>
+                <Tablerow r1={r1}>
+                 {Tablerow c1 = c1></Tablerow[c1]>}
+
+            </TableRow>
+        <\Table>
+    <\TableContainer>
+
+
+
+
+    );
+
+}
+
+export default WorkoutResults;
