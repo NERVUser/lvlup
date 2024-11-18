70d4ba3f96a5558b0e70720d69a590350e586d2e
Logan <loganbialek@ufl.edu>
2024-11-18 18:27:43 -0500

    sideNave

--- frontend/src/SideNav/SideNav.tsx
+++ frontend/src/SideNav/SideNav.tsx
@@ -4,27 +4,39 @@ import MenuIcon from "@mui/icons-material/FitnessCenter";
 import FastfoodIcon from '@mui/icons-material/Restaurant';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import RecommendIcon from '@mui/icons-material/Recommend';
+import { IconButton, Button } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
+interface SideNavProps {
+  onClose?: () => void;
+}
 
-function SideNav() {
+function SideNav({ onClose }: SideNavProps) {
   return (
     <div className="SideNav">
+      <div className="SideNavHeader">
+        {onClose && (
+          <IconButton onClick={onClose} className="CloseButton">
+            <CloseIcon />
+          </IconButton>
+        )}
+      </div>
       <div className="MenuOptions">
         <div className="MenuOptionsItem">
           <MenuIcon className="MenuBar" />
-          <p>Workout</p>
+          <Button className="MenuButton" style={{ color: "#ffffff" }}>Workout</Button>
         </div>
         <div className="MenuOptionsItem">
           <FastfoodIcon className="MenuBar" />
-          <p>Food</p>
+          <Button className="MenuButton" style={{ color: "#ffffff" }}>Food</Button>
         </div>
         <div className="MenuOptionsItem">
           <AccountCircleIcon className="MenuBar" />
-          <p>Account</p>
+          <Button className="MenuButton" style={{ color: "#ffffff" }}>Account</Button>
         </div>
         <div className="MenuOptionsItem">
           <RecommendIcon className="MenuBar" />
-          <p>Recommendations</p>
+          <Button className="MenuButton" style={{ color: "#ffffff" }}>Recommendations</Button>
         </div>
       </div>
     </div>
