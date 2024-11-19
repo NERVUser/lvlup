b74decebe47abaf8ef5faab7be60bde5b1f0d08e
Neil S <88115444+jul158@users.noreply.github.com>
2024-11-19 17:34:01 -0500

    Update Recommendations.tsx
    need to update for api used

--- frontend/src/Recommendations.tsx
+++ frontend/src/Recommendations.tsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import '.\recommendations.css';
+import all from '@mui'
 
+ const getrecs = async () =>
+ {
+    const response = await fetch('api here')
+    if (!response)
+    {
+        break;
+    }
+ }
+
+return (
+    <div className = "Holder">
+        {recommendations.map(recommendations) =>
+            <div key = recommendation.id} className = recommendations_list>
+                <h1>
+                    {actual_recommendation}
+                </h1>
+                    
+            
+            </div>}
+    </div>
+    
+)
+
+
+
+
+export default recommendations;
