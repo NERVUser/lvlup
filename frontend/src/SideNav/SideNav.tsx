3d1399f65dc9cf305d8826a8f7c9adf5fd1bc6e4
Stephen Smith <95247593+stephendsmith22@users.noreply.github.com>
2024-11-19 15:43:05 -0500

    updated side bar

--- frontend/src/SideNav/SideNav.tsx
+++ frontend/src/SideNav/SideNav.tsx
@@ -2,43 +2,6 @@ import MenuIcon from "@mui/icons-material/FitnessCenter";
 import FastfoodIcon from '@mui/icons-material/Restaurant';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import RecommendIcon from '@mui/icons-material/Recommend';
-<<<<<<< HEAD
-import { IconButton, Button } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close";
-
-interface SideNavProps {
-  onClose?: () => void;
-}
-
-function SideNav({ onClose }: SideNavProps) {
-  return (
-    <div className="SideNav">
-      <div className="SideNavHeader">
-        {onClose && (
-          <IconButton onClick={onClose} className="CloseButton">
-            <CloseIcon />
-          </IconButton>
-        )}
-      </div>
-      <div className="MenuOptions">
-        <div className="MenuOptionsItem">
-          <MenuIcon className="MenuBar" />
-          <Button className="MenuButton" style={{ color: "#ffffff" }}>Workout</Button>
-        </div>
-        <div className="MenuOptionsItem">
-          <FastfoodIcon className="MenuBar" />
-          <Button className="MenuButton" style={{ color: "#ffffff" }}>Food</Button>
-        </div>
-        <div className="MenuOptionsItem">
-          <AccountCircleIcon className="MenuBar" />
-          <Button className="MenuButton" style={{ color: "#ffffff" }}>Account</Button>
-        </div>
-        <div className="MenuOptionsItem">
-          <RecommendIcon className="MenuBar" />
-          <Button className="MenuButton" style={{ color: "#ffffff" }}>Recommendations</Button>
-        </div>
-      </div>
-=======
 import React from 'react';
 import { Button, Menu, MenuItem } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
@@ -110,7 +73,6 @@ function SideNav() {
         </MenuItem>
         <MenuItem onClick={() => navigate('/Leaderboard')}>Leaderboard</MenuItem>
       </Menu>
->>>>>>> Journals
     </div>
   )
 }
