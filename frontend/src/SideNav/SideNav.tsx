e13504c25b28cfad4f6e210387df4835bfc95608
Logan <loganbialek@ufl.edu>
2024-12-01 11:34:19 -0500

    Sidenave updates

--- frontend/src/SideNav/SideNav.tsx
+++ frontend/src/SideNav/SideNav.tsx
@@ -1,18 +1,16 @@
-import MenuIcon from "@mui/icons-material/FitnessCenter";
+import MenuIcon from "@mui/icons-material/Menu";
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import FastfoodIcon from '@mui/icons-material/Restaurant';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import RecommendIcon from '@mui/icons-material/Recommend';
 import LeaderboardIcon from '@mui/icons-material/Leaderboard';
 import React from 'react';
-import { Button, Menu, MenuItem, Box } from '@mui/material';
+import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { createTheme, ThemeProvider } from "@mui/material";
 
 function SideNav() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
   const navigate = useNavigate();
 
   const theme = createTheme({
@@ -26,84 +24,108 @@ function SideNav() {
             gap: 10
           }
         }
+      },
+      MuiListItemText: {
+        styleOverrides: {
+          primary: {
+            color: '#ffffff',
+            fontWeight: 'bold',
+          }
+        }
+      },
+      MuiDrawer: {
+        styleOverrides: {
+          paper: {
+            backgroundColor: '#333333',
+          }
+        }
       }
-      
-    }
-  })
+    },
+    palette: {
+      primary: {
+        main: '#ffffff',
+      },
+    },
+  });
 
-  //used to open and close our menu
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  }
-  const handleClose = () => setAnchorEl(null);
+  // Used to open and close the drawer
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  };
 
-  const handleWorkoutClick = () => {
-    handleClose();
-    navigate('/WorkoutJournal');
-  }
-  const handleFoodClick = () => {
-    handleClose();
-    navigate('/FoodJournal');
-  }
-  const handleAccountClick = () => {
-    handleClose();
-    navigate('/Account');
-  }
-  const handleReccomendationsClick = () => {
-    handleClose();
-    navigate('/Recommendations');
-  }
-  const handleLeaderboardClick = () => {
-    handleClose();
-    navigate('/Leaderboard');
-  }
+  const handleNavigate = (path: string) => {
+    handleDrawerClose();
+    navigate(path);
+  };
 
   return (
     <ThemeProvider theme={theme}>
       <Box>
-        <Button
-          id='basic-button'
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup='true'
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClick}
+        {/* Hamburger Menu Icon */}
+        <IconButton
+          onClick={handleDrawerOpen}
           color="primary"
+          aria-label="open drawer"
         >
-          Dashboard
-        </Button>
-        <Menu
-          id='basic-menu'
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
+          <MenuIcon />
+        </IconButton>
+
+        {/* Drawer Component */}
+        <Drawer
+          anchor="left"
+          open={isDrawerOpen}
+          onClose={handleDrawerClose}
         >
-          <MenuItem onClick={handleWorkoutClick}>
-            <FitnessCenterIcon />
-            <p>Workout</p>
-          </MenuItem>
-          <MenuItem onClick={handleFoodClick}>
-            <FastfoodIcon />
-            <p>Food</p>
-          </MenuItem>
-          <MenuItem onClick={handleAccountClick}>
-            <AccountCircleIcon />
-            <p>Account</p>
-          </MenuItem>
-          <MenuItem onClick={handleReccomendationsClick}>
-            <RecommendIcon />
-            <p>Recommendations</p>
-          </MenuItem>
-          <MenuItem onClick={handleLeaderboardClick}>
-            <LeaderboardIcon />
-            <p>Leaderboard</p>
-          </MenuItem>
-        </Menu>
+          <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={handleDrawerClose}
+            onKeyDown={handleDrawerClose}
+          >
+            <List>
+              <ListItemButton onClick={() => handleNavigate('/WorkoutJournal')}>
+                <ListItemIcon>
+                  <FitnessCenterIcon sx={{ color: '#ffffff' }} />
+                </ListItemIcon>
+                <ListItemText primary="Workout" />
+              </ListItemButton>
+
+              <ListItemButton onClick={() => handleNavigate('/FoodJournal')}>
+                <ListItemIcon>
+                  <FastfoodIcon sx={{ color: '#ffffff' }} />
+                </ListItemIcon>
+                <ListItemText primary="Food" />
+              </ListItemButton>
+
+              <ListItemButton onClick={() => handleNavigate('/Account')}>
+                <ListItemIcon>
+                  <AccountCircleIcon sx={{ color: '#ffffff' }} />
+                </ListItemIcon>
+                <ListItemText primary="Account" />
+              </ListItemButton>
+
+              <ListItemButton onClick={() => handleNavigate('/Recommendations')}>
+                <ListItemIcon>
+                  <RecommendIcon sx={{ color: '#ffffff' }} />
+                </ListItemIcon>
+                <ListItemText primary="Recommendations" />
+              </ListItemButton>
+
+              <ListItemButton onClick={() => handleNavigate('/Leaderboard')}>
+                <ListItemIcon>
+                  <LeaderboardIcon sx={{ color: '#ffffff' }} />
+                </ListItemIcon>
+                <ListItemText primary="Leaderboard" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Drawer>
       </Box>
     </ThemeProvider>
-  )
+  );
 }
 
-export default SideNav
+export default SideNav;
